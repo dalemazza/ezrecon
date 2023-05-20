e8595bbfec0efaab8c8e9a0e4f3b2e03249c3ad6
dalemazza <66513243+dalemazza@users.noreply.github.com>
2023-05-20 20:00:53 +0100

    Update ezrecon.sh
    updated!

--- ezrecon.sh
+++ ezrecon.sh
@@ -33,15 +33,48 @@ conduct_nikto_scan() {
     fi
 }
 
+# Help function
+display_help() {
+    echo "Usage: $0 [OPTIONS] <IP address | file>"
+    echo "OPTIONS:"
+    echo "  -h, --help           Display this help message and exit"
+    echo "  -d, --directory DIR  Specify the directory name to store the results"
+    echo
+    echo "EXAMPLES:"
+    echo "  $0 192.168.1.100           # Scan a single IP address"
+    echo "  $0 ips.txt                # Scan a list of IP addresses from a file"
+    echo "  $0 -d scan_results 192.168.1.100   # Specify a custom directory name"
+    exit 0
+}
+
 # Main script
 
-# Check the number of arguments
-if [[ $# -eq 0 ]]; then
-    echo "Usage: $0 <IP address | file>"
-    exit 1
+# Check if no arguments are provided or if the first argument is "--help" or "-h"
+if [[ $# -eq 0 || "$1" == "--help" || "$1" == "-h" ]]; then
+    display_help
 fi
 
-# Process the argument
+# Set default directory name
+directory_name="scan_results"
+
+# Process the arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -d|--directory)
+            directory_name="$2"
+            shift 2
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+# Create the directory to store the results
+mkdir -p "$directory_name"
+
+# Process the remaining argument(s)
 if [[ -f "$1" ]]; then
     # Argument is a file containing a list of IPs
     ip_file=$1
@@ -51,38 +84,11 @@ if [[ -f "$1" ]]; then
         echo "Scanning IP: $ip"
         
         # Create a subdirectory for the IP within the main directory
-        output_dir="$ip"
+        output_dir="$directory_name/$ip"
         mkdir -p "$output_dir"
         
         # Conduct the nmap scan
         conduct_nmap_scan "$ip" "$output_dir"
         
         # Conduct the version and OS scan
-        conduct_version_os_scan "$ip" "$output_dir"
-        
-        # Conduct the nikto scan on ports 80 and 443
-        conduct_nikto_scan "$ip" "$output_dir"
-        
-        echo "Scan completed for IP: $ip"
-        echo
-    done < "$ip_file"
-else
-    # Argument is a single IP address
-    ip=$1
-    echo "Scanning IP: $ip"
-    
-    # Create a subdirectory for the IP within the main directory
-    output_dir="$ip"
-    mkdir -p "$output_dir"
-    
-    # Conduct the nmap scan
-    conduct_nmap_scan "$ip" "$output_dir"
-    
-    # Conduct the version and OS scan
-    conduct_version_os_scan "$ip" "$output_dir"
-    
-    # Conduct the nikto scan on ports 80 and 443
-    conduct_nikto_scan "$ip" "$output_dir"
-    
-    echo "Scan completed for IP: $ip"
-fi
+        conduct_version_os_scan "$ip"
